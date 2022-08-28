cb8b8bcddad26e2922e2b854b0a85fd9d5ac2c34
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-08-28 19:31:57 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+    let form = document.querySelector('form')
+    let id = document.getElementById("searchByID");
+    form.addEventListener ('submit', (e)=>{
+        e.preventDefault()
+        fetch( `http://localhost:3000/movies/${id.value}`)
+        .then(response => response.json())
+        .then(data=>{
+          let section =  document.getElementById("movieDetails")
+          section.innerHTML = `
+          <h4>${data.title}</h4>
+          <p>${data.summary}</p>`
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
